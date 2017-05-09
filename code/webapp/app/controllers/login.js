7ec97a7441335d5ae89cefb6fad52ae96c310f88
Mirza Vučijak <Mirza Vuijak>
2017-05-10 01:41:55 +0200

    Remove temporary variables

--- code/webapp/app/controllers/login.js
+++ code/webapp/app/controllers/login.js
@@ -3,7 +3,6 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
     isToSAccepted: Ember.computed.not('isToSCheckboxChecked'),
     model: {},
-    success: "nopee",
     actions: {
         register() {
             let _userSvc = this.get('userSvc');
