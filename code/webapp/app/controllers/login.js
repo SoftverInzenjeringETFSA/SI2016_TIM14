5472e9102cd263064837dc2532dfc8d0cf37a653
etfgunner <milan.zuza18@gmail.com>
2017-05-11 15:46:58 +0200

    validacija

--- code/webapp/app/controllers/login.js
+++ code/webapp/app/controllers/login.js
@@ -1,13 +1,53 @@
 import Ember from 'ember';
-
 export default Ember.Controller.extend({
     isToSAccepted: Ember.computed.not('isToSCheckboxChecked'),
     model: {},
     actions: {
         register() {
-            let _userSvc = this.get('userSvc');
-
-            _userSvc.register(this.model, (resp) => alert("success"));
+			check();
+            
         }
+		,
+		check() {
+			
+			var regUsername = /^[a-zA-Z0-9]+$/;
+			var wasError=false;
+			if(!this.get('model.username').match(regUsername))
+			{
+				this.set('errorUsername','Username can contain only letters and numbers!');
+				wasError=true;
+			}
+			else
+			this.set('errorUsername','');	
+            var regEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+			if(!this.get('model.email').match(regEmail))
+			{
+				this.set('errorEmail','Email must be in example@something.com!');
+				wasError=true;
+			}
+			else
+			this.set('errorEmail','');
+			var regPassword = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+			if(!this.get('model.password').match(regPassword))
+			{
+				this.set('errorPassword','Password must contain at least one number, uppercase letter, lowercase letter and at least 6 characters!');
+				wasError=true;
+			}
+			else this.set('errorPassword','');
+			if(this.get('model.password')!=this.get('model.confirmedPassword'))
+			{
+				this.set('errorConfirmedPassword','Passwords do not match!');
+				wasError=true;
+			}
+			else this.set('errorConfirmedPassword','');
+				
+			if(!wasError)
+			{
+				let _userSvc = this.get('userSvc');
+            _userSvc.register(this.model, (resp) => alert("success"));
+			}
+        }
+		
+		
     }
 });
---
+++ code/webapp/app/controllers/users/new.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+	actions: {
+		register: function()
+	{
+		var username= this.get('model.username');
+		alert('username');
+	}
+	}
+});
