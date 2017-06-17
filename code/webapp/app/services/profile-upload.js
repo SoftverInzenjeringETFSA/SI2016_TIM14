02c75e0476825664b7554477b54886f22c9747d8
emilisic1 <emilisic1@etf.unsa.ba>
2017-06-17 21:17:55 +0200

    deployment - sitne izmjene

---
+++ code/webapp/app/services/profile-upload.js
@@ -0,0 +1,6 @@
+import Ember from 'ember';
+import CloudinaryProfileUpload from 'ember-profile-upload/services/cloudinary-profile-upload';
+
+export default CloudinaryProfileUpload.extend();
+export default Ember.Service.extend({
+});
